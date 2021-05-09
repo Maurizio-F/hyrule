8ce91b30b290275240449204d6aba2281af4293a
Maurizio-F <end2zone@googlemail.com>
2021-05-09 20:29:11 +0200

    Add Box Shadow and Zoom effect

--- components/character.js
+++ components/character.js
@@ -3,7 +3,7 @@ import { createElement } from "../utils/elements";
 
 export function createCharacterElement({ id, image, name, description }) {
   return createElement("section", {
-    className: "card",
+    className: "card block",
     children: [
       createElement("img", { src: image }),
       createElement("h2", { innerText: name }),
